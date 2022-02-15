8d3c682ded9110b1ea88f2a2d26135a23215ca2d
Bruno Eduardo <brunoeduardozh@gmail.com>
2022-02-15 10:33:09 -0300

    fix: users seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,8 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
-
+// use Faker\Generator as Faker;
+use App\Models\User;
 class UserSeeder extends Seeder
 {
     /**
@@ -10,14 +10,8 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run(Faker $faker)
+    public function run()
     {
-        return [
-            'name' => $faker->name,
-            'email' => $faker->unique()->safeEmail,
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
+        factory(User::class, 10)->create();
     }
 }
